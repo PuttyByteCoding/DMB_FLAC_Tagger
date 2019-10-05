691ae8c3af3f86c9818666fd98e3b3ce51f911dc
Putty <mark9745va@gmail.com>
2019-10-04 22:16:10 -0400

    Added flask_restful library. Added DataTypes.data_types classes. Added Flask render_template and request.

--- src/app.py
+++ src/app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api
+from DataTypes.data_types import Song, SongList
 app = Flask(__name__)
 
 # Flask Settings
 app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
+api = Api(app)
+
+
 
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
+api.add_resource(Song, '/song/<string:name>')
+api.add_resource(SongList, '/songs/')
 
 if __name__ == '__main__':
-    app.run(port=7777)
+    app.run(port=7777, debug=True)
