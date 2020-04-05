4922a6f88becd3b608353f0ed2b3aa414218b724
Putty <mark9745va@gmail.com>
2020-04-05 12:09:08 -0400

    Updated to new plural endpoint. Added songs ingestion. Added additonal logging.

--- src/ingest_external_info.py
+++ src/ingest_external_info.py
@@ -11,6 +11,8 @@ def main():
         shows_count = len(show_list)
         for show in show_list:
             shows_processed += 1
+
+            # Process Venue Info
             logger.info(f"Processing show {shows_processed} of {shows_count}")
             try:
                 venue_dict = {
@@ -27,17 +29,48 @@ def main():
                 logger.error(f"Could not parse show {show['venue'][1]}")
             add_venue_to_database(venue_dict)
 
+            # Process Guest Info
+
+            # Process Songs From Antsmarching
+            try:
+                songs_list = []
+                for song in show['setlist']:
+                    song_dict = {'name': song['song_name']}
+                    songs_list.append(song_dict)
+                if len(songs_list) > 0:
+                    add_songs_to_database(songs_list)
+                else:
+                    logger.warning(f"No songs in this setlist: {show['setlist']}")
+            except:
+                logger.error(f"Could not parse songs from setlist. {show['setlist']}")
+
+
+def add_songs_to_database(songs_list):
+    session = requests.Session()
+    request = session.post(f'{config.webserver_for_api}/songs/',
+                           headers={
+                               'Content-type': 'application/json',
+                               'Accept': 'text/plain'},
+                           data=json.dumps(songs_list))
+    if request.status_code == 200:
+        logger.info(f"Added Songs to database,")
+        return True
+    else:
+        logger.warning(f"Failed to add Songs to database.")
+        return False
 
 def add_venue_to_database(venue_dict):
     session = requests.Session()
-    request = session.post(f'{config.webserver_for_api}/venue/',
+    request = session.post(f'{config.webserver_for_api}/venues/',
                            headers={
                                'Content-type': 'application/json',
                                'Accept': 'text/plain'},
                            data=json.dumps(venue_dict))
     if request.status_code == 200:
+        logger.info(f"Added Venue to database: {venue_dict['name']}")
         return True
     else:
+        logger.warning(f"Failed to add Venue to database: {venue_dict['name']}")
         return False
 
 
