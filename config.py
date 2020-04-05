9994493f6867b7e6dce51b7b13b4d4f4211d45d0
Putty <mark9745va@gmail.com>
2020-04-04 22:04:36 -0400

    Added URL for API

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 webserver_for_files = "http://127.0.0.1:5555"
 local_webserver_dir = "/Volumes/500GB_SSD/02-ready_to_tag/"
 
+webserver_for_api = "http://127.0.0.1:6666"
+
 path_of_concerts_to_ingest = "/Volumes/500GB_SSD/01-originals"
 database_username = "root"
 database_password = ""
